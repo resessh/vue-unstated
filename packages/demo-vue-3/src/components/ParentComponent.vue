96302a84ed1bbb8242c01b725c4ac6a62a918c73
resessh <info@resessh.net>
2020-09-17 21:23:02 +0900

    chore: add a demo project using Vue3

---
+++ packages/demo-vue-3/src/components/ParentComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Parent</p>
+    <p>Count: {{ state.count }}</p>
+    <hr />
+    <ChildComponent />
+  </div>
+</template>
+
+<script>
+import ChildComponent from './ChildComponent.vue';
+import { counterContainer } from '@/useCounter';
+
+export default {
+  name: 'ParentComponent',
+  components: {
+    ChildComponent,
+  },
+  setup() {
+    const { state } = counterContainer.useContainer();
+
+    return {
+      state,
+    };
+  },
+};
+</script>
---
+++ packages/demo-vue-3/src/useCounter.js
@@ -0,0 +1,14 @@
+import { reactive } from 'vue';
+import { createContainer } from 'vue-unstated';
+
+const useCounter = (initialState = { count: 0 }) => {
+  const state = reactive(initialState);
+
+  const increment = () => {
+    state.count++;
+  };
+
+  return { state, increment };
+};
+
+export const counterContainer = createContainer(useCounter);
